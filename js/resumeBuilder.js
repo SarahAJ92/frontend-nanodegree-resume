54eb2565ffa1cbf8fef15325305901990400649a
SarahAJ92 <sarah.aljathlan@gmail.com>
2017-08-12 11:26:28 +0300

    update work project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,5 +11,43 @@ var bio = {
 "biopic" : "images/SJ.jpg",
 "welcomeMessage" : "welcome to Sarah Aljathlan porfolio!",
 "skills" : ["Awesomeness","Analysis","Reading"]
-
 };
+
+
+var education = [
+"schools" : {
+	"name":"imam muhammad bin saud islamic university",
+	"location":"Riyadh",
+	"degree": "bachelor",
+	"major": ["computer science and information systems","computer science"],
+	"dates": "23-05-2017";
+	"url": "";
+},
+"onlineCourses" : {
+"title": "fend",
+"school": "udacity"
+"dates": ""
+"url": "https://classroom.udacity.com/nanodegrees/nd001/parts/last-viewed"
+},
+];
+
+var work = {
+	      "jobs": ["IT support", "ux desinger", "information architecture"]
+           "employer": "ux ui desinger",
+           "title": "ux ui desinger",
+           "location": "Riyadh",
+           "dates": "2015-2016-2017",
+           "description": "work with ux reaserch and user interface",
+};
+
+
+var projects = {
+            "title": "string" 
+            "dates": "2016"
+            "description": string
+            "images": array with string urls
+};
+
+bio.display = function()
+education.display = function()
+work.display = function()
