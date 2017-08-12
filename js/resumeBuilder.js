76903aa5f7a619197c62028bf212658fbd791d63
SarahAJ92 <sarah.aljathlan@gmail.com>
2017-08-12 10:52:27 +0300

    update bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- consol.log("test");
+var bio = {
+"name" : "Sarah Aljathlan",
+"role" : "UX/Ui desinger",
+"contacts" : {
+	"mobile" : "123-213-11",
+	"email" : "sarah.aljathlan@gmail.com",
+	"github" : "SarahAJ92",
+	"twitter" : "@isaaso",
+	"location" : "Riyadh"
+},
+"biopic" : "images/SJ.jpg",
+"welcomeMessage" : "welcome to Sarah Aljathlan porfolio!",
+"skills" : ["Awesomeness","Analysis","Reading"]
+
+};
